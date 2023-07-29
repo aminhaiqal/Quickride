d0077c0670b45445ca05a026be24a2f50265834b
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-07-29 15:29:47 +0800

    add color theme

---
+++ quickride/lib/widgets/theme.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ColorTheme {
+  static ThemeData get lightTheme {
+
+    final ColorScheme customColorScheme = ColorScheme(
+    primary: Color(0xFFEA600B),
+    secondary: Color(0xFF2978A0),
+    surface: Colors.white,
+    background: Colors.white,
+    error: Color(0xFFB00030),
+    onPrimary: Colors.white,
+    onSecondary: Colors.black,
+    onSurface: Colors.black,
+    onBackground: Colors.black,
+    onError: Colors.white,
+    brightness: Brightness.light,
+  );
+    return ThemeData(
+      colorScheme: customColorScheme,
+    );
+  }
+
+  static ThemeData get darkTheme {
+
+    final ColorScheme customColorScheme = ColorScheme(
+    primary: Color(0xFFF37629),
+    secondary: Color(0xFF3980A4),
+    surface: Color(0xFF121212),
+    background: Color(0xFF121212),
+    error: Colors.red,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.white,
+    onBackground: Colors.white,
+    onError: Colors.white,
+    brightness: Brightness.dark,
+  );
+    return ThemeData(
+      colorScheme: customColorScheme,
+    );
+  }
+}
+
+class TextTheme {
+  static TextStyle get headline1 {
+    return GoogleFonts.poppins(
+      fontSize: 56,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle get headline2 {
+    return GoogleFonts.montserrat(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle get headline3 {
+    return GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle get body1 {
+    return GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      letterSpacing: 0,
+    );
+  }
+
+  static TextStyle get body2 {
+    return GoogleFonts.montserrat(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0,
+    );
+  }
+}
