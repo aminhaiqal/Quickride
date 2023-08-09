9849e4933f9991ba8942b407d1910b3097c1cc91
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-08-09 10:23:53 +0800

    add route

--- quickride/lib/screens/register/register_view.dart
+++ quickride/lib/screens/register/register_view.dart
@@ -11,51 +11,58 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Center(
-            child: Padding(
-                padding: const EdgeInsets.only(top: 68),
-                child: Column(children: <Widget>[
-                  Image.asset(
-                    '/images/logo.png',
-                    height: 100,
-                    width: 100,
-                  ),
-                  const SizedBox(height: 20),
-                  Text('Sign Up',
-                      style: textStyle.TextTheme.headline2(FontWeight.w600)),
-                  const SizedBox(height: 8),
-                  Text('Create your first account',
-                      style: textStyle.TextTheme.body1(null).copyWith(
-                        color: Colors.black.withOpacity(0.5),
-                      )),
-                  const SizedBox(height: 48),
-                  const textField.TextField(
-                      label: 'Username', prefixIcon: Icon(Icons.person)),
-                  const SizedBox(height: 12),
-                  const textField.TextField(
-                      label: 'Phone Number', prefixIcon: Icon(Icons.phone)),
-                  const SizedBox(height: 12),
-                  const textField.TextField(
-                      label: 'Email', prefixIcon: Icon(Icons.email)),
-                  const SizedBox(height: 12),
-                  const textField.PasswordTextField(),
-                  const SizedBox(height: 84),
-                  const actionButton.PrimaryButton(
-                      label: 'Sign Up', width: 380, onPressed: null),
-                  const SizedBox(height: 16),
-                  RichText(
-                      text: TextSpan(
-                          style: textStyle.TextTheme.body1(null).copyWith(
-                              color: theme.ColorTheme.mainTheme.colorScheme
-                                  .onBackground),
-                          children: const <TextSpan>[
-                        TextSpan(text: 'Already have an account? '),
-                        TextSpan(
-                            text: 'Sign In',
-                            style: TextStyle(fontWeight: FontWeight.bold))
-                      ])),
-                  const SizedBox(height: 86),
-                ]))));
+    return Material(
+        child: Scaffold(
+            body: SingleChildScrollView(
+                child: Center(
+                    child: Padding(
+                        padding: const EdgeInsets.fromLTRB(24, 68, 24, 86),
+                        child: Column(children: <Widget>[
+                          Image.asset(
+                            '/images/logo.png',
+                            height: 100,
+                            width: 100,
+                          ),
+                          const SizedBox(height: 20),
+                          Text('Sign Up',
+                              style: textStyle.TextTheme.headline2(
+                                  FontWeight.w600)),
+                          const SizedBox(height: 8),
+                          Text('Create your first account',
+                              style: textStyle.TextTheme.body1(null).copyWith(
+                                color: Colors.black.withOpacity(0.5),
+                              )),
+                          const SizedBox(height: 48),
+                          const textField.TextField(
+                              label: 'Username',
+                              prefixIcon: Icon(Icons.person)),
+                          const SizedBox(height: 12),
+                          const textField.TextField(
+                              label: 'Phone Number',
+                              prefixIcon: Icon(Icons.phone)),
+                          const SizedBox(height: 12),
+                          const textField.TextField(
+                              label: 'Email', prefixIcon: Icon(Icons.email)),
+                          const SizedBox(height: 12),
+                          const textField.PasswordTextField(),
+                          const SizedBox(height: 84),
+                          const actionButton.PrimaryButton(
+                              label: 'Sign Up', width: 380, onPressed: null),
+                          const SizedBox(height: 16),
+                          RichText(
+                              text: TextSpan(
+                                  style:
+                                      textStyle.TextTheme.body1(null).copyWith(
+                                    color: theme.ColorTheme.mainTheme
+                                        .colorScheme.onBackground,
+                                  ),
+                                  children: const <TextSpan>[
+                                TextSpan(text: 'Already have an account? '),
+                                TextSpan(
+                                    text: 'Sign In',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                              ]))
+                        ]))))));
   }
 }
