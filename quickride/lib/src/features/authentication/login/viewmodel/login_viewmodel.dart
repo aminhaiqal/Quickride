8bee2689cf7d00f61b791f94850c36ee5924a8db
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-09-07 17:34:11 +0800

    remove unnecessary function

--- quickride/lib/src/features/authentication/login/viewmodel/login_viewmodel.dart
+++ quickride/lib/src/features/authentication/login/viewmodel/login_viewmodel.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/foundation.dart';
 import '../../data/repository/authentication_repository.dart';
-import 'package:quickride/src/features/authentication/data/repository/authentication_repository.dart' as auth_repo;
-
 class LoginViewModel with ChangeNotifier {
   String email = '';
   String password = '';
@@ -14,35 +12,5 @@ class LoginViewModel with ChangeNotifier {
   void setPassword(String value) {
     password = value;
     notifyListeners();
-  }
-
-  Future<String?> firebaseLogin() async {
-    try{
-      String? message = await auth_repo.AuthenticationRepository.firebaseSignIn(email, password);
-      return message;
-    } catch (e) {
-      print(e);
-      return e.toString();
-    }
-  }
-
-  Future<bool> facebookLogin() async {
-    try{
-      await AuthenticationRepository.signInWithFacebook();
-    } catch (e) {
-      print(e);
-      return false;
-    }
-    return true;
-  }
-
-  Future<bool> googleLogin() async {
-    try{
-      await AuthenticationRepository.signInWithGoogle();
-    } catch (e) {
-      print(e);
-      return false;
-    }
-    return true;
   }
 }
