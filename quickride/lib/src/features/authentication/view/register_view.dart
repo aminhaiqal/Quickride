89f5161772bba1cea7e1d5d7879f69d2b6291f08
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-11-13 23:28:47 +0800

    Refactor register view to use separate header and footer widgets

--- quickride/lib/src/features/authentication/view/register_view.dart
+++ quickride/lib/src/features/authentication/view/register_view.dart
@@ -12,50 +12,63 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AuthViewModel>(
-            create: (_) => AuthViewModel(),
-            child: Consumer<AuthViewModel>(
-                builder: (context, viewModel, child) {
-              return SingleChildScrollView(
-                  child: Center(
-                child: Column(children: <Widget>[
-                  Container(
-                      margin: const EdgeInsets.only(top: 48, bottom: 64),
-                      child: Column(
-                        children: [
-                          Text('Sign Up',
-                              style: shared.TextTheme.headline2(
-                                  FontWeight.w600)),
-                          Text('Create your account',
-                              style: shared.TextTheme.description(null)
-                                  .copyWith(color: const Color(0xFF8C8C8C)))
-                        ],
-                      )),
-                  RegisterCredential(viewModel: viewModel),
-                  Container(
-                      margin: const EdgeInsets.only(top: 16),
-                      child: RichText(
-                          text: TextSpan(children: <TextSpan>[
-                        TextSpan(
-                            text: 'Already have an account? ',
-                            style: shared.TextTheme.description(
-                                    FontWeight.w400)
-                                .copyWith(
-                                    color: shared.ColorTheme.mainTheme
-                                        .colorScheme.onBackground
-                                        .withOpacity(0.5))),
-                        TextSpan(
-                            recognizer: TapGestureRecognizer()..onTap = () {
-                              Navigator.pushNamed(context, '/login');
-                            },
-                            text: 'Sign In',
-                            style: shared.TextTheme.description(
-                                    FontWeight.w500)
-                                .copyWith(
-                                    color: shared.ColorTheme.mainTheme
-                                        .colorScheme.primary))
-                      ])))
-                ]),
-              ));
-            }));
+        create: (_) => AuthViewModel(),
+        child: Consumer<AuthViewModel>(
+          builder: (context, viewModel, child) {
+            return SingleChildScrollView(
+                child: Center(
+                    child: Column(
+              children: <Widget>[
+                _SignUpHeader(),
+                RegisterCredential(viewModel: viewModel),
+                _SignInFooter(),
+              ],
+            )));
+          },
+        ));
+  }
+}
+
+class _SignUpHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 48, bottom: 64),
+        child: Column(
+          children: [
+            Text('Sign Up', style: shared.TextTheme.headline2(FontWeight.w600)),
+            Text('Create your account',
+                style: shared.TextTheme.description(null)
+                    .copyWith(color: const Color(0xFF8C8C8C))),
+          ],
+        ));
+  }
+}
+
+class _SignInFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 16),
+        child: RichText(
+            text: TextSpan(
+          children: <TextSpan>[
+            TextSpan(
+              text: 'Already have an account? ',
+              style: shared.TextTheme.description(FontWeight.w400).copyWith(
+                  color: shared.ColorTheme.mainTheme.colorScheme.onBackground
+                      .withOpacity(0.5)),
+            ),
+            TextSpan(
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.pushNamed(context, '/login');
+                },
+              text: 'Sign In',
+              style: shared.TextTheme.description(FontWeight.w500).copyWith(
+                  color: shared.ColorTheme.mainTheme.colorScheme.primary),
+            )
+          ],
+        )));
   }
 }
